88b86c53654731dbf8a6c6002018bd1200856e1b
Kamron H <19638873+kamronthopkins@users.noreply.github.com>
2022-04-03 01:52:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
@@ -93,3 +94,150 @@ print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
 print(f'{"2 : Make Withdraw":20s} {"3 : Option 4":16s}')
 print(f'{"4 : Edit Profile":20s} {"5 : Exit":16s}')
 """
+=======
+import datetime
+from admin import run_admin_program
+from account_holders import accounts
+
+# help('FORMATTING')
+
+
+def run_user_program(user):
+    """
+    THIS FUNCTION IS THE USER PROGRAM.
+    After the PIN is verified, this program will run.
+    The User Object/Dictionary is passed into this function to be used for CRUD.
+    """
+    print(user['pin'])
+    print(user)
+    print(user['pin'])
+    date = str(datetime.date.today())
+    # Display user-name & date
+    print(f"\n\n{user['last']:8s}{user['first']:14s} {date:8s}")
+    # Display Divider
+    print('-' * 35)
+
+    print(f'{"0 : Accounts":20} {"1 : Deposit":16s}')
+    print(f'{"2 : Transfer":20s} {"3 : Settings":16s}')
+    print(f'{"4 : Products":20s} {"5 : Exit":16s}\n')
+
+    user_input = input('Enter your selection:\n').strip().lower()
+    print('\n\n\n\n\n')
+
+    if user_input != 'exit':
+        if user_input == '0' or user_input == 'accounts':
+            # Display user-name & date
+            print(f'{user["last"]:8s} {user["first"]:14s} {date:8s}')
+            # Display Divider
+            print('-' * 35)
+
+            print(f'{"Current Balance":<15} : {user["debit"]:>10}\n')
+            print(f'{"1 : View Transactions":25s} {"3 : Credit":16s}')
+            print(f'{"4 : Profile":25s} {"5 : Exit":16s}\n')
+            user_input = input('Enter your selection:\n')
+
+            # If the user chooses the 'EXIT' command, restart/re-run the program.
+            if user_input == '5' or user_input == 'exit':
+                # Restart/re-run the program to get back to Main Menu
+                run_user_program(accounts)
+
+        if user_input == '1' or user_input == 'deposit':
+            # Display user-name & date
+            print(f"{user['last']:8s}{user['first']:14s} {date:8s}")
+            # Display Divider
+            print('-' * 35)
+
+            print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
+            print(f'{"2 : Saving":20s} {"3 : Credit":16s}')
+            print(f'{"4 : Profile":20s} {"5 : Exit":16s}\n')
+            user_input = input('Enter your selection:\n')
+
+            # If the user chooses the 'EXIT' command, restart/re-run the program.
+            if user_input == '5' or user_input == 'exit':
+                # Restart/re-run the program to get back to Main Menu
+                run_user_program(accounts)
+
+        if user_input == '2' or user_input == 'transfer':
+            # Display user-name & date
+            print(f"{user['last']:8s}{user['first']:14s} {date:8s}")
+            # Display Divider
+            print('-' * 35)
+
+            print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
+            print(f'{"2 : Saving":20s} {"3 : Credit":16s}')
+            print(f'{"4 : Profile":20s} {"5 : Exit":16s}\n')
+            user_input = input('Enter your selection:\n')
+
+            # If the user chooses the 'EXIT' command, restart/re-run the program.
+            if user_input == '5' or user_input == 'exit':
+                # Restart/re-run the program to get back to Main Menu
+                run_user_program(accounts)
+
+        if user_input == '3' or user_input == 'settings':
+            # Display user-name & date
+            print(f"{user['last']:8s}{user['first']:14s} {date:8s}")
+            # Display Divider
+            print('-' * 35)
+
+            print(f'{"0 : Checking":20} {"1 : Deposit":16s}')
+            print(f'{"2 : Saving":20s} {"3 : Credit":16s}')
+            print(f'{"4 : Profile":20s} {"5 : Exit":16s}\n')
+            user_input = input('Enter your selection:\n')
+
+            # If the user chooses the 'EXIT' command, restart/re-run the program.
+            if user_input == '5' or user_input == 'exit':
+                # Restart/re-run the program to get back to Main Menu
+                run_user_program(accounts)
+
+        if user_input == '4' or user_input == 'products':
+            # Display Divider
+            print('-' * 35)
+
+            print(f'{"Savings Interest :"}')
+            print(f'{"CD Interest :"}')
+            print(f'{"Business Loan Rates :"}\n')
+
+            # Return Command
+            print(f'{"0 : Return to Main Menu":20}\n')
+            # Ask Admin for Next Command
+            user_input = input('Enter your selection:\n')
+
+            # If the user chooses the 'EXIT' command, restart/re-run the program.
+            if user_input == '0' or user_input == 'exit':
+                # Restart/re-run the program to get back to Main Menu
+                print('\n\n\n\n\n')
+                # start_atm()
+                run_admin_program(accounts)
+    print('Thank you for using XYZ Bank. Have a great day!')
+
+
+def start_atm():
+    # WELCOME MESSAGE (START OF PROGRAM)
+    print(f'{"Welcome to College Bank":^50s}\n\n')
+
+    # Request User-Account PIN
+    entered_pin = (input('Enter Your PIN: ')).strip()
+
+    # Check entered PIN against all Account-PIN's by looping
+    for account in accounts.values():
+        # print(account['pin'])
+        pin_ref = account['pin']
+
+        # If PIN's match, run User_Program(). Passing the individual account data of matching PIN.
+        if int(pin_ref) == int(entered_pin):
+            # print(account)
+            # Run user program/function
+            run_user_program(account)
+
+            # TODO: COME UP W/ A WAY TO CLOSE PROGRAM ON COMMAND (EXIT FUNCTION)
+
+            # if int(entered_pin) == pin:
+            #     run_user_program(account)
+            # elif entered_pin == 0000:
+            #     print('admin pin entered')
+            # else:
+            #     print('Sorry Invalid PIN, Try Again...')
+
+
+start_atm()
+>>>>>>> Stashed changes
