ccbd271bad798624ca32d4659af9029105d5172f
Kamron H <19638873+kamronthopkins@users.noreply.github.com>
2022-04-03 01:52:09 -0400

    Update account_holders.py

---
+++ account_holders.py
@@ -0,0 +1,121 @@
+# Accounts Dictionary
+accounts = {
+    'Mutulu Shakur': {
+        'pin': 1111,
+        'first': 'Matulu',
+        'last': 'Shakur',
+        'dob': '00/00/00',
+        'debit': 7430,
+        'saving': 247,
+        'credit': 900,
+        'credit_used': 113,
+        'address': '1884 ECU Blvd.',
+        'state': '1884 ECU Blvd.',
+        'debit_transactions': {
+            'date': '00/00/00',
+            'debit_credit': 'debit',
+            'expense': 'random expense',
+            'amount_debited_credited': '-45.00',
+            'amount_remaining': '1155.00'
+        },
+        'savings_transactions': {
+            'date': '00/00/00',
+            'debit_credit': 'debit',
+            'expense': 'random expense',
+            'amount_debited_credited': '-45.00',
+            'amount_remaining': '1155.00'
+        },
+        'credit_transactions': {
+            'date': '00/00/00',
+            'debit_credit': 'debit',
+            'expense': 'random expense',
+            'amount_debited_credited': '-45.00',
+            'amount_remaining': '1155.00'
+        }
+    },
+    'Jacques Kallis': {
+        'pin': 2222,
+        'first': 'Jacques',
+        'last': 'Kallis',
+        'dob': '00/00/00',
+        'debit': 200,
+        'saving': 247,
+        'credit': 900,
+        'credit_used': 113,
+        'address': '1884 ECU Blvd.',
+        'state': '1884 ECU Blvd.',
+        'debit_transactions': {
+            'date': '00/00/00',
+            'debit_credit': 'debit',
+            'expense': 'random expense',
+            'amount_debited_credited': '-50.00',
+            'amount_remaining': '1150.00'
+        },
+        'savings_transactions': {
+            'date': '00/00/00',
+            'debit_credit': 'debit',
+            'expense': 'random expense',
+            'amount_debited_credited': '-20.00',
+            'amount_remaining': '1180.00'
+        },
+        'credit_transactions': {
+            'date': '00/00/00',
+            'debit_credit': 'debit',
+            'expense': 'random expense',
+            'amount_debited_credited': '-145.00',
+            'amount_remaining': '1055.00'
+        }
+    },
+    'Ricky Bobby': {
+        'pin': 3333,
+        'first': 'Ricky',
+        'last': 'Bobby',
+        'dob': '00/00/00',
+        'debit': 3000,
+        'saving': 247,
+        'credit': 900,
+        'credit_used': 113,
+        'address': '1884 ECU Blvd.',
+        'state': 'NC',
+        'debit_transactions': {
+            'date': '00/00/00',
+            'debit_credit': 'debit',
+            'expense': 'random expense',
+            'amount_debited_credited': '-45.00',
+            'amount_remaining': '1155.00'
+        },
+        'savings_transactions': {
+            'date': '00/00/00',
+            'debit_credit': 'debit',
+            'expense': 'random expense',
+            'amount_debited_credited': '-45.00',
+            'amount_remaining': '1155.00'
+        },
+        'credit_transactions': {
+            'date': '00/00/00',
+            'debit_credit': 'debit',
+            'expense': 'random expense',
+            'amount_debited_credited': '-45.00',
+            'amount_remaining': '1155.00'
+        }
+    },
+}
+
+# transaction = ['date', 'debit or credit', 'random expense', 'amount debited/credited', 'remaining amount']
+
+# # Creation of User (to be used later w/ create-user view in admin)
+# accounts['User_1'] = {
+#     'pin': 0000,
+#     'first': 'Matulu',
+#     'last': 'Shakur',
+#     'dob': '00/00/00',
+#     'debit': 3218,
+#     'saving': 247,
+#     'credit': 900,
+#     'credit_used': 113,
+#     'address': '1884 ECU Blvd.',
+#     'state': '1884 ECU Blvd.',
+#     'debit_transactions': [],
+#     'savings_transactions': [],
+#     'credit_transactions': []
+# }
